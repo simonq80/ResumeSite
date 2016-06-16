649d271a494cd2b0a5a725ddb72f3b7fe59d2718
Simon Quigley <simon.quigley@domain1.zamano.com>
2016-06-16 11:01:36 +0100

    added cv & home pages, moved most data to json files

--- app/app.js
+++ app/app.js
@@ -1,9 +1,52 @@
-var phonecatApp = angular.module('projectApp', []);
+var projectApp = angular.module('projectApp', ['ngRoute']);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('ProjectController', ['$scope', '$http', function PhoneListController($scope, $http) {
+projectApp.controller('ProjectController', ['$scope', '$http', function PhoneListController($scope, $http) {
 
-	$http.get('projects.json').then(function(response) {
+	$http.get('data/pages.json').then(function(response) {
+        $scope.pages = response.data;
+      });
+
+	$http.get('data/projects.json').then(function(response) {
         $scope.projects = response.data;
       });
+	$http.get('data/homeParas.json').then(function(response) {
+        $scope.homeParas = response.data;
+      });
 }]);
+
+projectApp.config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/projects', {
+          template: '<my-data></my-data>'
+        }).
+        when('/cv', {
+          template: '<my-cv></my-cv>'
+        }).
+        when('/home', {
+          template: '<my-home></my-home>'
+        }).
+        otherwise('/home');
+    }
+]);
+
+projectApp.directive('myData', function() {
+	return{
+		templateUrl: 'project.template.html'
+	};
+});
+
+projectApp.directive('myCv', function() {
+	return{
+		templateUrl: 'cv.template.html'
+	};
+});
+
+projectApp.directive('myHome', function() {
+	return{
+		templateUrl: 'home.template.html'
+	};
+});
